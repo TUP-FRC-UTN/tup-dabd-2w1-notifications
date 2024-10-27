e793feb347bd9f9c6d53523e63763cb990c50198
NicolasPerloUtn <113969@tecnicatura.frc.utn.edu.ar>
2024-10-27 17:25:05 -0300

    Change Notification Register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AllNotificationComponent } from "./components/all-notification/all-notification.component";
 //import {components} from './components'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AllNotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/all-notification/all-notification.component.ts
+++ src/app/components/all-notification/all-notification.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationRegisterService } from '../../service/notification-register.service';
+import { Notifications } from '../../models/notifications';
 import { Access } from '../../models/access';
 import { Fine } from '../../models/fine';
-import { General } from '../../models/general';
 import { Payments } from '../../models/payments';
-import { Notifications } from '../../models/notifications';
+import { General } from '../../models/general';
+import $ from 'jquery';
+import 'datatables.net'
+import 'datatables.net-bs5';
+import { style } from '@angular/animations';
+import "datatables.net-select"
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-all-notification',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './all-notification.component.html',
   styleUrl: './all-notification.component.css'
 })
@@ -17,6 +23,18 @@ export class AllNotificationComponent implements OnInit{
 
   ngOnInit(): void {
     this.llenarData();
+    $('#myTable').DataTable({
+      select: {
+        style: 'multi'
+    },
+      paging: true,
+      searching: true,
+      ordering: true,
+      lengthChange: true,
+      pageLength: 10,
+      
+
+    });
   }
 
   constructor(private service: NotificationRegisterService) {}
@@ -25,8 +43,6 @@ export class AllNotificationComponent implements OnInit{
   originalFinesList:Fine[] = []
   originalPaymentsList:Payments[] = []
   originalGeneralsList:General[]= []
-  startDate: Date = new Date();
-  endDate: Date= new Date();
   data: Notifications = {
     fines: [],
     access: [],
@@ -43,11 +59,33 @@ export class AllNotificationComponent implements OnInit{
         this.originalPaymentsList = [...value.payments]
         this.originalGeneralsList = [...value.generals]
 
+        this.fillTable(); 
         console.log(value)
       },
       error: ()=> {
         alert('error al cargar las notifications')
       }
      })
+  }
+
+  fillTable() {
+    let table = $("#myTable").DataTable();
+    for (let notification of this.data.access) {
+    const date = notification.date as { [key: string]: any }
+    let dateString = date[0] + "-" + date[1] + "-" + date[2] + "  " + date[3] + ":" + date[4] + ":" + date[5]
+      table.row.add([notification.subject, notification.description, dateString, notification.nombre + " " + notification.apellido, notification.dni]).draw(false);
+    }
+    for (let notification of this.data.fines) {
+
+      table.row.add([notification.subject, notification.description, notification.date, notification.nombre + " " + notification.apellido, notification.dni]).draw(false);
+    }
+    for (let notification of this.data.payments) {
+
+      table.row.add([notification.subject, notification.description, notification.date, notification.nombre + " " + notification.apellido, notification.dni]).draw(false);
+    }
+    for (let notification of this.data.generals) {
+
+      table.row.add([notification.subject, notification.description, notification.date, notification.nombre + " " + notification.apellido, notification.dni]).draw(false);
+    }
   }
 }
